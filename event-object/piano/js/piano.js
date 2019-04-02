7a08a87a9b1a2b127315d58f6312b241b63c8064
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-02 11:26:02 +0300

    Пианино

--- event-object/piano/js/piano.js
+++ event-object/piano/js/piano.js
@@ -1,29 +1,30 @@
 const lowerList = [
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/first.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/second.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/third.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/fourth.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/fifth.mp3'
+	'sounds/lower/first.mp3',
+	'sounds/lower/second.mp3',
+	'sounds/lower/third.mp3',
+	'sounds/lower/fourth.mp3',
+	'sounds/lower/fifth.mp3'
 ];
 
 const middleList = [
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/first.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/second.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/third.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/fourth.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/fifth.mp3'
+	'sounds/middle/first.mp3',
+	'sounds/middle/second.mp3',
+	'sounds/middle/third.mp3',
+	'sounds/middle/fourth.mp3',
+	'sounds/middle/fifth.mp3'
 ]
 
 const higherList = [
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/first.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/second.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/third.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/fourth.mp3',
-	'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/fifth.mp3'
+	'sounds/higher/first.mp3',
+	'sounds/higher/second.mp3',
+	'sounds/higher/third.mp3',
+	'sounds/higher/fourth.mp3',
+	'sounds/higher/fifth.mp3'
 ];
 
-const pianoKeys = Array.from(document.getElementsByClassName('set')[0].getElementsByTagName('li'));
-console.log(pianoKeys);
+const piano = document.getElementsByClassName('set')[0];
+
+const pianoKeys = Array.from(piano.getElementsByTagName('li'));
 
 function player(event, i) {
 	const keyAudio = event.currentTarget.getElementsByTagName('audio')[0];
@@ -40,5 +41,32 @@ function player(event, i) {
 for (let i = 0; i < pianoKeys.length; i++) {
 	pianoKeys[i].addEventListener('click', function () {
 		player(event, i)
-	});
+  });
+  
+}
+
+document.addEventListener('keydown', lowerMode);
+function lowerMode(event) {
+	if (event.shiftKey) {
+    piano.classList.remove('middle');
+		piano.classList.add('lower');
+  };
+  
+  if (event.altKey) {
+    piano.classList.remove('middle');
+		piano.classList.add('higher');
+  }
+  
+}
+
+document.addEventListener('keyup', middleMode);
+function middleMode(event) {
+  if (!event.shiftKey) {
+    piano.classList.remove('lower');
+		piano.classList.add('middle');
+  };
+  if (!event.altKey) {
+    piano.classList.remove('higher');
+		piano.classList.add('middle');
+  };
 }
