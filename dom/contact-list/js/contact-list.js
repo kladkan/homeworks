6c60e306a53618fe2333d231c5c487a546909d49
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-03 21:02:55 +0300

    Задача 1. Список контактов

---
+++ dom/contact-list/js/contact-list.js
@@ -0,0 +1,11 @@
+const contacts = JSON.parse(loadContacts());
+
+const contactsList = document.querySelector('.contacts-list');
+
+const contactsArray = [];
+
+contacts.forEach(contact => {
+  contactsArray.push('<li data-email="' + contact.email + '" data-phone="' +  contact.phone + '"><strong>' + contact.name + '</strong></li>');
+});
+
+contactsList.innerHTML = contactsArray.join('');
