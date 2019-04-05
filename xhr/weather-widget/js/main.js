f36274cfe903e324c5d6946750be66fdc3dada33
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-05 23:03:07 +0300

    Задача 1. Виджет погоды

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,14 +1,33 @@
+/* начальное состояние кода
+const request = new XMLHttpRequest();
+request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+request.send();
+if (request.status === 200) {
+  const response = JSON.parse(request.responseText);
+  setData(response);
+}
+*/
+
+//Решение:
 const request = new XMLHttpRequest();
 
-request.addEventListener("load", onLoad); // проверка загрузился ли запрос
+request.addEventListener("load", onLoad);
+request.addEventListener("error", onError);
 
 request.open('GET', 'https://neto-api.herokuapp.com/weather', true);
 request.send();
 
 function onLoad() {
-
+  console.log("Сработало событие load");
   if (request.status === 200) {
+    console.log(`Ответ ${request.status}, статус: ${request.statusText}`);
     const response = JSON.parse(request.responseText);
     setData(response);
+  } else if (request.status !== 200) {
+    console.log(`Ответ ${request.status}, статус: ${request.statusText}`);
   }
+}
+
+function onError() {
+  console.log("Сработало событие error (сетевая ошибка, сервер не доступен)");
 }
