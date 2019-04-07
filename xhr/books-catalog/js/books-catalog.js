4cb193ccbcbe0ec45c37d657979f0f59a307286c
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-07 19:21:33 +0300

    Задача 3. Каталог книг

--- xhr/books-catalog/js/books-catalog.js
+++ xhr/books-catalog/js/books-catalog.js
@@ -1,31 +1,34 @@
-const requestBooks = new XMLHttpRequest();
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+  const requestBooks = new XMLHttpRequest();
 
-requestBooks.addEventListener('load', onLoad);
-requestBooks.addEventListener('error', onError);
+  requestBooks.addEventListener('load', onLoad);
+  requestBooks.addEventListener('error', onError);
 
-requestBooks.open('GET', 'https://neto-api.herokuapp.com/book/', true);
-requestBooks.send();
+  requestBooks.open('GET', 'https://neto-api.herokuapp.com/book/', true);
+  requestBooks.send();
 
-function onLoad() {
-  console.log('Сработало событие load');
-  if (requestBooks.status === 200) {
-    const books = JSON.parse(requestBooks.responseText);
-    setData(books);
-  }
-  console.log(`Ответ ${requestBooks.status}, статус: ${requestBooks.statusText}`);
-};
+  function onLoad() {
+    console.log('Сработало событие load');
+    if (requestBooks.status === 200) {
+      const books = JSON.parse(requestBooks.responseText);
+      setLiTags(books);
+    }
+    console.log(`Ответ ${requestBooks.status}, статус: ${requestBooks.statusText}`);
+  };
 
-function onError() {
-  console.log('Сработало событие error (сетевая ошибка, сервер не доступен)');
-};
+  function onError() {
+    console.log('Сработало событие error (сетевая ошибка, сервер не доступен)');
+  };
 
-const booksContent = [];
+  const booksContent = [];
 
-function setData(books) {
+  function setLiTags(books) {
 
-  for (const book of books) {
-    booksContent.push(`<li data-title="${book.title}" data-author="${book.author.name}" data-info="${book.info}" data-price="${book.price}"><img src="${book.cover.small}"></li>`);
-  }
-  document.getElementById('content').innerHTML = booksContent.join('');
-};
+    for (const book of books) {
+      booksContent.push(`<li data-title="${book.title}" data-author="${book.author.name}" data-info="${book.info}" data-price="${book.price}"><img src="${book.cover.small}"></li>`);
+    }
+    document.getElementById('content').innerHTML = booksContent.join('');
+  };
 
+}
