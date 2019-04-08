98023d9381db33433cab4046cec9e2201f1aa67c
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-08 13:02:10 +0300

    Задача 1. Список дел

---
+++ html-forms/todo-list/js/todo-list.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', check);
+const inputs = document.getElementsByTagName('input');
+for (const inp of inputs) {
+  inp.addEventListener('click', check);
+};
+
+const totalOutput = document.getElementsByTagName('output')[0];
+const listBlock = document.querySelector('.list-block');
+
+function check() {
+  const total = [];
+  for (const inp of inputs) {
+    if (inp.checked) {
+      total.push('ok');
+    };
+    totalOutput.innerHTML = total.length;
+  };
+  if (total.length === inputs.length) {
+    listBlock.classList.add('complete');
+  } else {
+    listBlock.classList.remove('complete');
+  };
+}
