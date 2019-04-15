a557779fba9ba682347c71534e5516196eed62be
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-15 08:46:11 +0300

    Задача 2. Сортировка таблицы - Доработал

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -4,7 +4,6 @@ function handleTableClick(event) {
 
   if (event.target.tagName.toLowerCase() === 'th') {
     event.target.dataset.dir = event.target.dataset.dir === '1' ? '-1' : '1';
-    console.log(event.target);
     event.currentTarget.dataset.sortBy = event.target.dataset.propName;
     sortTable(event.target.dataset.propName, event.target.dataset.dir);
   }
