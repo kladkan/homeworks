1781d470f3f378053715a5df359e9dc95e5c544f
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-16 22:10:58 +0300

    Задача 2. Форма авторизации и регистрации

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const signInForm = document.querySelector('.sign-in-htm');
-const signUnForm = document.querySelector('.sign-up-htm');
-const signInFormData = new FormData(signInForm);
+const signUpForm = document.querySelector('.sign-up-htm');
 
 const loginWrap = document.querySelector('.login-wrap');
 loginWrap.addEventListener('click', formsAction);
@@ -10,19 +9,32 @@ loginWrap.addEventListener('click', formsAction);
 function formsAction(event) {
   if (event.target.value === 'Войти') {
     const signInFormData = new FormData(signInForm);
-    const xhr = new XMLHttpRequest()
+    const xhrSignIn = new XMLHttpRequest()
 
-    xhr.addEventListener('load', (e) => {
-      console.log(xhr.responseText);
-      event.target.parentElement.nextElementSibling.textContent = 'Пользователь Иван успешно авторизован';
+    xhrSignIn.open('POST', 'https://neto-api.herokuapp.com/signin');
+    xhrSignIn.setRequestHeader('Content-Type', 'application/json');
+    xhrSignIn.send(JSON.stringify(signInFormData));
+
+    xhrSignIn.addEventListener('load', (e) => {
+      console.log(xhrSignIn);
+      event.target.parentElement.nextElementSibling.textContent = JSON.parse(xhrSignIn.responseText).error ? JSON.parse(xhrSignIn.responseText).message : 'Пользователь Иван успешно авторизован';
     });
-    xhr.addEventListener('error', (e) => {
-      console.log(xhr.responseText);
-    });
-    xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(signInFormData));
   };
+
+  if (event.target.value === 'Зарегистрироваться') {
+    event.preventDefault();
+    const signUpFormData = new FormData(signUpForm);
+    const xhrSignUp = new XMLHttpRequest()
+
+    xhrSignUp.open('POST', 'https://neto-api.herokuapp.com/signup');
+    xhrSignUp.setRequestHeader('Content-Type', 'application/json');
+    xhrSignUp.send(JSON.stringify(signUpFormData));
+
+    xhrSignUp.addEventListener('load', (e) => {
+      event.target.parentElement.nextElementSibling.textContent = JSON.parse(xhrSignUp.responseText).error ? JSON.parse(xhrSignUp.responseText).message : `Пользователь ${JSON.parse(xhrSignUp.responseText).name} успешно зарегистрирован`;
+    });
+  };
+
 }
 
 
