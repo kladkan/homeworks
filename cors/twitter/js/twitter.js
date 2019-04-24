af3e7c271dd749909c1969036685913bfff66493
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-24 07:32:30 +0300

    Задача 1. Твиттер-виджет

---
+++ cors/twitter/js/twitter.js
@@ -0,0 +1,36 @@
+'use strict';
+/*
+function callback(data) {
+  console.log(data);
+}
+*/
+//======================================
+
+function randName(max, min) {
+  return `callback${Math.floor(Math.random() * (max - min)) + min}`;
+}
+
+function loadData(url) {
+  const functionName = randName(100, 1000);
+  return new Promise((done, fail) => {
+    window[functionName] = done;
+
+    const script = document.scripts[0].cloneNode();
+    script.src = `${url}?jsonp=${functionName}`;
+    document.body.appendChild(script);
+  });
+}
+
+function showData(twitterUser) {
+  console.log(twitterUser);
+  /*
+  const target = document.querySelector('.book');
+  target.innerHTML = `Книга ${book.title}, автор ${book.author.name}`;
+  */
+}
+
+loadData('https://neto-api.herokuapp.com/twitter/')
+  .then(showData)
+  .catch(error => console.log(error));
+  
+
