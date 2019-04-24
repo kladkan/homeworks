cf059be5bfdc58c587fca042d6d303e3ead2f29c
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-24 10:51:37 +0300

    Задача 1. Твиттер-виджет

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,37 +1,40 @@
 'use strict';
-/*
-function callback(data) {
-  console.log(data);
-}
-*/
-//======================================
+
+const dataWallpaper = document.querySelector('.bio').firstElementChild;
+const dataUsername = document.querySelector('.desc').firstElementChild;
+const datadescription = document.querySelector('.desc').lastElementChild;
+const dataPic = document.querySelector('.avatarcontainer > .avatar');
+const contentData = document.querySelector('.content > .data');
+const dataTweets = contentData.querySelector('[data-tweets]');
+const dataFollowers = contentData.querySelector('[data-followers]');
+const dataFollowing = contentData.querySelector('[data-following]');
 
 function randName(max, min) {
   return `callback${Math.floor(Math.random() * (max - min)) + min}`;
 }
 
 function loadData(url) {
-  const functionName = randName(100, 1000);
+  const functionName = randName(10, 1000);
   return new Promise((done, fail) => {
     window[functionName] = done;
 
-    const script = document.scripts[0].cloneNode();
+    //const script = document.scripts[0].cloneNode();
+    const script = document.createElement('script');
     script.src = `${url}?jsonp=${functionName}`;
     document.body.appendChild(script);
   });
 }
 
-function showData(twitterUser) {
-  console.log('Проверка');
-
-  /*
-  const target = document.querySelector('.book');
-  target.innerHTML = `Книга ${book.title}, автор ${book.author.name}`;
-  */
+function showData(data) {
+  dataWallpaper.src = data.wallpaper;
+  dataUsername.textContent = data.username;
+  datadescription.textContent = data.description;
+  dataPic.src = data.pic;
+  dataTweets.textContent = data.tweets;
+  dataFollowers.textContent = data.followers;
+  dataFollowing.textContent = data.following;
 }
 
 loadData('https://neto-api.herokuapp.com/twitter/jsonp')
   .then(showData)
   .catch(error => console.log(error));
-  
-
