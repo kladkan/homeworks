d5c19c71194fd370a0acf4ba1623d5b587c1599c
kladkan <43966184+kladkan@users.noreply.github.com>
2019-04-28 00:32:57 +0300

    Задача 2. Рисование в браузере

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,65 +1,40 @@
 'use strict';
 
-let brushRadius = 5;
-
 const canvas = document.getElementById('draw');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let curves = [];
+let brushRadius = 100;
+let curvePointsCoord = [];
 let drawing = false;
-let needsPaint = false;
 let hue = 0;
+let direction;
 
-function circle(point) {
+function circle(x, y) {
+  curvePointsCoord.push([x, y]);
   ctx.beginPath();
-  ctx.arc(...point, brushRadius / 2, 0, 2 * Math.PI);
-
-  ctx.fillStyle = `hsl(${hue},100%,25%)`;
+  ctx.arc(x, y, brushRadius / 2, 0, 2 * Math.PI);
+  ctx.fillStyle = `hsl(${hue},100%,50%)`;
   ctx.fill();
+
 }
 
-function smoothCurveBetween(p1, p2) {
-  // Bezier control point
-  const cp = p1.map((coord, idx) => (coord + p2[idx]) / 2);
-  ctx.quadraticCurveTo(...p1, ...cp);
-}
-
-function smoothCurve(points) {
-  ctx.beginPath();
-  ctx.lineWidth = brushRadius;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-
-  ctx.moveTo(...points[0]);
-
-  for (let i = 1; i < points.length - 1; i++) {
-    smoothCurveBetween(points[i], points[i + 1]);
-  }
-  
-  ctx.strokeStyle = `hsl(${hue},100%,25%)`;
-  ctx.stroke();
-}
 
 window.addEventListener('resize', (evt) => {
   canvasClear();
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
 });
 
 canvas.addEventListener('mousedown', (evt) => {
   drawing = true;
-  const curve = [];
-  
-  curve.push([evt.offsetX, evt.offsetY]);
-  curves.push(curve);
-  needsPaint = true;
+  circle(evt.offsetX, evt.offsetY);
+
 });
 
 canvas.addEventListener('mouseup', (evt) => {
   drawing = false;
+  curvePointsCoord = [];
 });
 
 canvas.addEventListener('mouseleave', (evt) => {
@@ -70,36 +45,53 @@ canvas.addEventListener('dblclick', canvasClear);
 
 canvas.addEventListener('mousemove', (evt) => {
   if (drawing) {
-    const point = [evt.offsetX, evt.offsetY];
-    curves[curves.length - 1].push(point);
-    needsPaint = true;
+    curvePointsCoord.push([evt.offsetX, evt.offsetY]);
+
+
+
+    if (curvePointsCoord.length === 3) {
+      ctx.beginPath();
+      
+      ctx.lineWidth = brushRadius;
+      
+      if (brushRadius === 100 || direction === 'minus') {
+        brushRadius--;
+        direction = 'minus';
+      }
+     
+      if (brushRadius === 4 || direction === 'plus') {
+        brushRadius++;
+        direction = 'plus';
+      }
+
+      ctx.lineJoin = 'round';
+      ctx.lineCap = 'round';
+
+      ctx.moveTo(...curvePointsCoord[0]);
+
+      if (hue === 360) {
+        hue = 0;
+      };
+      if (hue === -1) {
+        hue = 359
+      };
+      
+      ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+      evt.shiftKey ? hue-- : hue++;
+
+      ctx.quadraticCurveTo(...curvePointsCoord[1], ...curvePointsCoord[2]);
+      ctx.stroke();
+    } else if (curvePointsCoord.length === 4) {
+      curvePointsCoord = curvePointsCoord.splice(2);
+    }
+
   }
 });
 
 function canvasClear() {
   drawing = false;
-  curves = [];
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  needsPaint = false;
+  curvePointsCoord = [];
+  brushRadius = 100;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
-
-function paint() {
-  curves
-    .forEach((curve) => {
-      circle(curve[0]);
-
-      smoothCurve(curve);
-    });
-}
-
-function tick() {
-  if (needsPaint) {
-    hue++;
-    console.log(hue);
-    paint();
-    needsPaint = false;
-  }
-  window.requestAnimationFrame(tick);
-}
-
-tick();
