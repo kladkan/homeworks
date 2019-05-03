5d76bac297734d2cabd0039b3c68ebe5fbaaeecd
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-03 23:58:13 +0300

    Задача 1. Посетители сайта в реальном времени

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -13,34 +13,14 @@ const realtime = new Chart(ctx).Bar({
 
 let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
+
 ws.addEventListener('message', event => {
-/*
+  let info = JSON.parse(event.data);
   if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
-
+    info.forEach(data => realtime.addData([data.online], data.time));
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
     realtime.removeData();
-    realtime.addData([Number(data)], label);
-  }
-*/
-let info = JSON.parse(event.data);
-  if (isFirst) {
-    console.log(typeof info);
-    info.forEach(data => realtime.addData(data.online, data.time));
-
-    isFirst = false;
-  } else {
-    //const [label, data] = event.data.split('|');
-    realtime.removeData();
-    realtime.addData(info.online, info.time);
-    console.log(info.online);
-    console.log(typeof info.online);
-    console.log(info.time);
-    console.log(typeof info.time);
+    realtime.addData([info.online], info.time);
   }
 });
