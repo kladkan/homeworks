2e028fe652160dd7843391231610851a5e005690
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-04 18:53:06 +0300

    Задача 2. Коллективное рисование

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+editor.addEventListener('update', event => {
+  event.canvas.toBlob(blob => {
+    ws.send(blob);
+  });
+});
+
+
