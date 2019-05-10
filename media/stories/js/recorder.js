6fdf5e7649a393ddf9d00f728d85c625efe9fbba
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-10 17:22:21 +0300

    Задача 3. NetoStories

--- media/stories/js/recorder.js
+++ media/stories/js/recorder.js
@@ -37,10 +37,59 @@ function createThumbnail(video) {
 }
 
 function record(app) {
+  console.log(app.config);
   return new Promise((done, fail) => {
     app.mode = 'preparing';
+    navigator.mediaDevices.getUserMedia(app.config)
+      .then((stream) => {
+        app.preview.srcObject = stream;
+        app.preview.onloadedmetadata = (e) => {
+          app.preview.play();
+        };
+        app.mode = 'recording';
+
+        app.preview.addEventListener('canplay', (event) => {
+          setTimeout(() => {
+            let recorder = new MediaRecorder(stream);
+            let chunks = [];
+
+            recorder.addEventListener('dataavailable',
+              (e) => chunks.push(e.data)
+            );
+
+            recorder.addEventListener('stop', (e) => {
+              const recorded = new Blob(chunks, { type: recorder.mimeType });
+              chunks = null;
+              recorder = stream = null;
+              app.preview.srcObject = null;// отключаем предварительный просмотр с камеры
+              stream.getTracks().forEach(track => track.stop());// выключаем камеру
+
+              const res = {};
+              res.video = recorded;
+              createThumbnail(recorded) // получаем кадр из видео (по заданию)
+                .then(pict => {
+                  res.frame = pict
+                  done(res);
+                });
+            });
+
+            recorder.start();
+            setTimeout(() => {// запуск таймера до окончания времени заданного в app.limit
+              recorder.stop();
+            }, app.limit);
+
+          }, 1000);
+        });
+
+      })
+      .catch(err => {
+        fail(err); //перевод промиса в статус rejected
+        console.warn(`Ошибка: ${err.message}`)
+      });
+    /*
     setTimeout(() => {
       fail('Не удалось записать видео');
     }, app.limit);
+    */
   });
 }
