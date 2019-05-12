1ac358db60fef8e5543c9105ac88a4850def0d1c
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-12 10:10:24 +0300

    Задача 1. Мусорная корзина

--- drag-and-drop/trash-bin/js/basket.js
+++ drag-and-drop/trash-bin/js/basket.js
@@ -4,25 +4,27 @@ let muvedLogo = null;
 let centerX = 0;
 let centerY = 0;
 
-document.addEventListener('mousedown', event => {
+const dragStart = event => {
   if (event.target.classList.contains('logo')) {
     muvedLogo = event.target;
     const logo = event.target.getBoundingClientRect();
+    // находим смещение от начала координат иконки до ее середины
     centerX = logo.width / 2;
     centerY = logo.height / 2;
   }
-});
+};
 
-document.addEventListener('mousemove', event => {
+const drag = throttle((x, y) => {
   if (muvedLogo) {
     event.preventDefault();// Предотвращаем выделение текста
-    muvedLogo.style.left = event.pageX - centerX + 'px';
-    muvedLogo.style.top = event.pageY - centerY + 'px';
+
+    muvedLogo.style.left = x - centerX + 'px';
+    muvedLogo.style.top = y - centerY + 'px';
     muvedLogo.classList.add('moving');
   }
 });
 
-document.addEventListener('mouseup', event => {
+const drop = event => {
   if (muvedLogo) {
     muvedLogo.style.visibility = 'hidden';
     const trashBin = document
@@ -32,6 +34,29 @@ document.addEventListener('mouseup', event => {
       muvedLogo.classList.remove('moving');
       muvedLogo.style.display = 'none';
       muvedLogo = null;
-    }
-  }
-});
+    };
+  };
+};
+
+//для десктопов
+document.addEventListener('mousedown', dragStart);
+document.addEventListener('mousemove', event => drag(event.pageX, event.pageY));
+document.addEventListener('mouseup', drop);
+
+//для сенсорных экранов
+document.addEventListener('touchstart', event => dragStart(event.touches[0]));
+document.addEventListener('touchmove', event => drag(event.touches[0].pageX, event.touches[0].pageY));
+document.addEventListener('touchend', event => drop(event.changedTouches[0]));
+
+function throttle(callback) {
+  let isWaiting = false;
+  return function () {
+    if (!isWaiting) {
+      callback.apply(this, arguments);
+      isWaiting = true;
+      requestAnimationFrame(() => {
+        isWaiting = false;
+      });
+    };
+  };
+};
