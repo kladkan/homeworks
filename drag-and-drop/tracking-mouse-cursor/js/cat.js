b054349df1f6c9b21023f34f7d80fa2f4fe2de9f
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-12 16:18:14 +0300

    Задача 2. Отслеживание указателя мыши

--- drag-and-drop/tracking-mouse-cursor/js/cat.js
+++ drag-and-drop/tracking-mouse-cursor/js/cat.js
@@ -1,25 +1,26 @@
 'use strict';
 
-let minDimention, // минимальное расстояние до края браузера от глаза
-    leftEye = document.querySelector('.cat_position_for_left_eye'), //глаз
-    leftPupil = document.querySelector('.cat_eye_left');// зрачок левого глаза
+let leftEye = document.querySelector('.cat_position_for_left_eye'), //глаз
+  leftPupil = document.querySelector('.cat_eye_left'),// зрачок левого глаза
+  dist;
 
 // координаты левого глаза
-const leftEyeX = leftEye.getBoundingClientRect().x;
-const leftEyeY = leftEye.getBoundingClientRect().y;
-
-leftEyeX > leftEyeY ?
-  minDimention = leftEyeY :
-  minDimention = leftEyeX;
+const leftEyeX = leftEye.getBoundingClientRect().x,
+  leftEyeY = leftEye.getBoundingClientRect().y;
 
 const moveLeftEye = throttle((x, y) => {
 
+  dist = Math.sqrt( // считаем текущее растояние от центра левого глаза до курсора
+    Math.pow((x - leftEyeX), 2) +
+    Math.pow((y - leftEyeY), 2)
+  )
+
   leftPupil.style.left = (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) +
-    (x - leftEyeX) / minDimention
+    (x - leftEyeX) / dist
     * (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) + 'px';
-  
+
   leftPupil.style.top = (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) +
-    (y - leftEyeY) / minDimention
+    (y - leftEyeY) / dist
     * (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) + 'px';
 
 });
