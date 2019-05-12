48069aff2358025a8fbaf749032ec1050ddfacaf
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-12 16:24:01 +0300

    Задача 2. Отслеживание указателя мыши

--- drag-and-drop/tracking-mouse-cursor/js/cat.js
+++ drag-and-drop/tracking-mouse-cursor/js/cat.js
@@ -2,7 +2,7 @@
 
 let leftEye = document.querySelector('.cat_position_for_left_eye'), //глаз
   leftPupil = document.querySelector('.cat_eye_left'),// зрачок левого глаза
-  dist;
+  distToLeftEye;
 
 // координаты левого глаза
 const leftEyeX = leftEye.getBoundingClientRect().x,
@@ -10,17 +10,17 @@ const leftEyeX = leftEye.getBoundingClientRect().x,
 
 const moveLeftEye = throttle((x, y) => {
 
-  dist = Math.sqrt( // считаем текущее растояние от центра левого глаза до курсора
+  distToLeftEye = Math.sqrt( // считаем текущее растояние от центра левого глаза до курсора
     Math.pow((x - leftEyeX), 2) +
     Math.pow((y - leftEyeY), 2)
   )
 
   leftPupil.style.left = (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) +
-    (x - leftEyeX) / dist
+    (x - leftEyeX) / distToLeftEye
     * (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) + 'px';
 
   leftPupil.style.top = (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) +
-    (y - leftEyeY) / dist
+    (y - leftEyeY) / distToLeftEye
     * (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) + 'px';
 
 });
