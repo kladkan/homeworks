09cc499bf95bfda73fce62f039d5fbad3c59aad6
kladkan <43966184+kladkan@users.noreply.github.com>
2019-05-18 20:48:09 +0300

    Задача 2. Отслеживание указателя мыши

--- drag-and-drop/tracking-mouse-cursor/js/cat.js
+++ drag-and-drop/tracking-mouse-cursor/js/cat.js
@@ -3,47 +3,53 @@
 let leftEye = document.querySelector('.cat_position_for_left_eye'), //левый глаз
   leftPupil = document.querySelector('.cat_eye_left'),// зрачок левого глаза
   distToLeftEye,// растояние от центра левого глаза до курсора
+
   rightEye = document.querySelector('.cat_position_for_right_eye'), //правый глаз
   rightPupil = document.querySelector('.cat_eye_right'),// зрачок правого глаза
   distToRightEye;// растояние от центра правого глаза до курсора
 
-// координаты левого глаза
-const leftEyeX = leftEye.getBoundingClientRect().x,
-  leftEyeY = leftEye.getBoundingClientRect().y,
 
-// координаты правого глаза
-  rightEyeX = rightEye.getBoundingClientRect().x,
-  rightEyeY = rightEye.getBoundingClientRect().y;
+const eyeRadius = leftEye.getBoundingClientRect().width / 2,// радиус глаза
+  pupilRadius = leftPupil.getBoundingClientRect().width / 2,// радиус зрачка
+  radius = eyeRadius - pupilRadius,// максимальное смещение зрачка от центра глаза
 
-const movePupil = throttle((x, y) => {
+  // координаты центра левого глаза
+  leftEyeX = leftEye.getBoundingClientRect().x + eyeRadius,
+  leftEyeY = leftEye.getBoundingClientRect().y + eyeRadius,
+
+  // координаты правого глаза
+  rightEyeX = rightEye.getBoundingClientRect().x + eyeRadius, // крорректировать
+  rightEyeY = rightEye.getBoundingClientRect().y + eyeRadius;
+
+const movePupil = throttle((x, y) => {// x и y - текущие координаты корсора относительно экрана
   distToLeftEye = Math.sqrt( // считаем текущее растояние от центра левого глаза до курсора
     Math.pow((x - leftEyeX), 2) +
     Math.pow((y - leftEyeY), 2)
   );
 
-  leftPupil.style.left = (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) +
+  leftPupil.style.left = (eyeRadius - pupilRadius) +
     (x - leftEyeX) / distToLeftEye
-    * (leftEye.offsetWidth / 2 - leftPupil.offsetWidth / 2) + 'px';
-
-  leftPupil.style.top = (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) +
+    * radius + 'px';
+ 
+  leftPupil.style.top = (eyeRadius - pupilRadius) +
     (y - leftEyeY) / distToLeftEye
-    * (leftEye.offsetHeight / 2 - leftPupil.offsetHeight / 2) + 'px';
+    * radius + 'px';
 
-  distToRightEye = Math.sqrt( // считаем текущее растояние от центра правого глаза до курсора
+    distToRightEye = Math.sqrt( // считаем текущее растояние от центра правого глаза до курсора
     Math.pow((x - rightEyeX), 2) +
     Math.pow((y - rightEyeY), 2)
   );
 
-  rightPupil.style.left = (rightEye.offsetWidth / 2 - rightPupil.offsetWidth / 2) +
+  rightPupil.style.left = (eyeRadius - pupilRadius) +
     (x - rightEyeX) / distToRightEye *
-    (rightEye.offsetWidth / 2 - rightPupil.offsetWidth / 2) + 'px';
+    radius + 'px';
 
-  rightPupil.style.top = (rightEye.offsetHeight / 2 - rightPupil.offsetHeight / 2) +
+  rightPupil.style.top = (eyeRadius - pupilRadius) +
     (y - rightEyeY) / distToRightEye *
-    (rightEye.offsetHeight / 2 - rightPupil.offsetHeight / 2) + 'px';
+    radius + 'px';
 });
 
-document.addEventListener('mousemove', event => movePupil(event.pageX, event.pageY));
+document.addEventListener('mousemove', event => movePupil(event.clientX, event.clientY));
 
 function throttle(callback) {
   let isWaiting = false;
